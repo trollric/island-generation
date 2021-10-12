7b7b268eb9f3a0d6aac215f169adf4d216ace4b1
Marcus Lilja <90m.hansson@gmail.com>
2021-10-12 10:11:08 +0200

    Updated legens with determine_trade_codes function

--- legend_creator.py
+++ legend_creator.py
@@ -1,6 +1,7 @@
 # Takes an UPP dictionary (Universal planetary profile) and cretes a map legend
 # This include descriptive name of the different colors, goverment type, temperature range,
 # Atmosphearic demands, Startport quality and trade codes.
+import json
 from planet_generator import create_color_palette
 from planet_generator import upp_to_dict
 
@@ -8,11 +9,18 @@ def generate(upp_dict, color_palette):
     #TODO: Gather necessary information
     #TODO: Create 
     pass
-    
+
+
+def determine_trade_codes(upp_dict):
+    with open("trade_code_classification.json",) as trade_requirement_data:
+        trade_requirements = json.load(trade_requirement_data)
+    #TODO: Start checking wich codes apply.
+
+
 def make_legend(upp_dict, color_palette, path):
     pass
     #TODO: Create the legend
-
+    #TODO: Determine trade codes
     #TODO: Save to path with <name>_legend
 
 
